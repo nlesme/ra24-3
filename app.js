c638189613eb70c7e664c649d6ba3cd1787f7275
Norbelis <nlesme244@gmail.com>
2025-07-09 19:08:06 -0500

    Fixed file

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ app.get('/', (req,res) => {
     res.send("Hello, World");
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+if (require.main === module) {
+    app.listen(port, () => {
+      console.log(`Server listening on http://localhost:${port}`);
+    });
+  }
 
 module.exports = app;
